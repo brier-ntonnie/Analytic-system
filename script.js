26dadb2bf3d3bb4e61f4b7be7344e987577beb8f
Ntonnie Brier <tonnykaranja420@gmail.com>
2025-03-06 21:18:59 +0300

    innitial commit of my first project

---
+++ script.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector('.wrapper');
+const loginlink = document.querySelector('.login-link');
+const registerlink = document.querySelector('.register-link');
+const btnpopup = document.querySelector('.btnlogin-popup');
+const btniconclose = document.querySelector('.icon-close');
+console.log("Wrapper:", wrapper);
+console.log("Login Link:", loginlink);
+console.log("Register Link:", registerlink);
+
+registerlink.addEventListener('click', () => {
+  console.log("Register link clicked!");
+  wrapper.classList.add('active');
+});
+
+loginlink.addEventListener('click', () => {
+  console.log("Login link clicked!");
+  wrapper.classList.remove('active');
+});
+btnpopup.addEventListener('click', () => {
+  console.log("btn popup clicked!");
+  wrapper.classList.add('active-popup');
+});
+iconclose.addEventListener('click', () => {
+  console.log("icon close clicked!");
+  wrapper.classList.remove('active-popup');
+});

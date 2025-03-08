5bc3407719534a49375fc268e96bee30ddef4078
Ntonnie Brier <tonnykaranja420@gmail.com>
2025-03-08 12:59:50 +0300

    second initial commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const wrapper = document.querySelector('.wrapper');
 const loginlink = document.querySelector('.login-link');
 const registerlink = document.querySelector('.register-link');
 const btnpopup = document.querySelector('.btnlogin-popup');
-const btniconclose = document.querySelector('.icon-close');
+const iconclose = document.querySelector('.icon-close');
 console.log("Wrapper:", wrapper);
 console.log("Login Link:", loginlink);
 console.log("Register Link:", registerlink);
